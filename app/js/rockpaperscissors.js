472777675c9cda2f32f2e2f6ed903bb0260fb068
Marque Reavley <WALTER@Marques-MacBook-Pro.local>
2016-05-12 20:20:09 -0400

    Took out extra semi-colons after player and computer move returns.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -30,7 +30,7 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return move; /* Your Expression */;
+    return move; /* Your Expression */
 }
 
 function getComputerMove(move) {
@@ -42,7 +42,7 @@ function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return move; /* Your Expression */;
+    return move; /* Your Expression */
 }
 
 function getWinner(playerMove,computerMove) {
